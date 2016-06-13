2d871d08c5c7ba63adc5b8660e95c92c2b1693c5
Christopher Porter <snooper90@gmail.com>
2016-06-13 12:29:10 -0500

    added and connected mongoose db

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+
+mongoose.connect(process.env.DB_CONN_TRIPTRAKER);
+
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
