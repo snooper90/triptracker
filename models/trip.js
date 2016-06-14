9fe4ffeec2fb206e9420670c472292706b7fe9bd
Christopher Porter <snooper90@gmail.com>
2016-06-14 09:18:17 -0500

    added trip model

---
+++ models/trip.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+    var tripSchema = new Schema({
+      name:{type: String, required: true}
+      start_date: { type: Date, required: true, unique: true },
+      end_date:{ type: Date, required: true, unique: true },
+      created_at: Date,
+      updated_at: Date
+      // should I list the dates between start and end?
+    });
+    // create created date and update apdated date
+    tripSchema.pre('save', function(next) {
+
+      var currentDate = new Date();
+      //update the updated_at
+      this.updated_at = currentDate;
+      //add created date
+      if (!this.created_at)
+        this.created_at = currentDate;
+
+      next();
+    });
+
+
+module.exports = mongoose.model('Trip', tripSchema);
