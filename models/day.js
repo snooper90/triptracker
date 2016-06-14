618aa5b66afef17390c697b47fd5a1ee8f5e64f7
Christopher Porter <snooper90@gmail.com>
2016-06-14 09:17:58 -0500

    add day model

---
+++ models/day.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+    // You should be able to get the days of the trip by selecting a date-span connected to the user.
+    var daySchema = new Schema({
+      date:{type: Date, required: true},
+      starting_point: {type: String, required: true},
+      destinations: {
+        address: [ String ],
+        distances:[ Number ],
+        discription:[ String ]
+      },
+      created_at: Date,
+      updated_at: Date
+    });
+
+    daySchema.pre('save', function(next) {
+      var currentDate = new Date();
+
+      this.updated_at = currentDate;
+
+      if (!this.created_at)
+        this.created_at = currentDate;
+
+      next();
+    });
+
+
+module.exports = mongoose.model('Day', daySchema);
